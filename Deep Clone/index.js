4cd26f6e50e3e313297f4179a23139601c61243b
RanungPark <ckehfqkr302@naver.com>
2024-02-02 20:16:20 +0900

    깊은 복사 함수

--- Deep Clone/index.js
+++ Deep Clone/index.js
@@ -1,5 +1,18 @@
-const add = (num1, num2) => {
-  return num1 + num2
+const cloneDeep = (target) => {
+  if (typeof target !== 'object' || target === null || target === undefined) {
+    return target;
+  }
+
+  if (Array.isArray(target)) {
+    return target.map(value => cloneDeep(value));
+  }
+
+  const cloneObj = {};
+  for (let key in target) {
+    cloneObj[key] = cloneDeep(target[key]);
+  }
+
+  return cloneObj;
 }
 
-export default add
+export default cloneDeep;
