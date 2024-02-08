cbace63b785a2a2ad491020ccac33a9a19b6c02c
RanungPark <ckehfqkr302@naver.com>
2024-02-08 14:38:38 +0900

    원시타입 복사

---
+++ Deep Clone/src/clonePrimitive.js
@@ -0,0 +1,16 @@
+import isBoolean from './comparators/isBoolean';
+import isNull from './comparators/isNull';
+import isNumber from './comparators/isNumber';
+import isString from './comparators/isString';
+import isUndefined from './comparators/isUndefined';
+import copyPrimitive from './copys/copyPrimitive';
+
+function clonePrimitive(value) {
+  if (isNumber(value) ||
+    isString(value) ||
+    isBoolean(value) ||
+    isNull(value) ||
+    isUndefined(value)) return copyPrimitive(value);
+}
+
+export default clonePrimitive;
