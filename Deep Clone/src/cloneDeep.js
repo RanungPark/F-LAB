7998602a7bdcd8a14ff52b075a03bbf77982748e
RanungPark <ckehfqkr302@naver.com>
2024-02-14 15:01:47 +0900

    객체를 이용하여 반복문 사용

--- Deep Clone/src/cloneDeep.js
+++ Deep Clone/src/cloneDeep.js
@@ -11,12 +11,74 @@
  * ---
  * 1. 복사 기능
  *  - Date, Set, Map, object, typedArray, regex, symbol
- */
+**/
+import isCyclic from './comparators/isCyclic';
+import isArray from './comparators/isArray';
+import isObject from './comparators/isObject';
+import isDate from './comparators/isDate';
+import isRegExp from './comparators/isRegExp';
+import isMap from './comparators/isMap';
+import isSet from './comparators/isSet';
+import isNumberObject from './comparators/isNumberObject';
+import isStringObject from './comparators/isStringObject';
+import isBooleanObject from './comparators/isBooleanObject';
 
-import cloneReference from './cloneReference';
+import copyArray from './copys/copyArray';
+import copyObject from './copys/copyObject';
+import copyDate from './copys/copyDate';
+import copyRegExp from './copys/copyRegExp';
+import copyMap from './copys/copyMap';
+import copySet from './copys/copySet';
+import copyNumberObject from './copys/copyNumberObject';
+import copyStringObject from './copys/copyStringObject';
+import copyBooleanObject from './copys/copyBooleanObject';
+
+const validations = [
+  {
+    validationr: isArray,
+    copy: copyArray,
+  },
+  {
+    validationr: isDate,
+    copy: copyDate,
+  },
+  {
+    validationr: isRegExp,
+    copy: copyRegExp,
+  },
+  {
+    validationr: isMap,
+    copy: copyMap,
+  },
+  {
+    validationr: isSet,
+    copy: copySet,
+  },
+  {
+    validationr: isNumberObject,
+    copy: copyNumberObject,
+  },
+  {
+    validationr: isStringObject,
+    copy: copyStringObject,
+  },
+  {
+    validationr: isBooleanObject,
+    copy: copyBooleanObject,
+  },
+  {
+    validationr: isObject,
+    copy: copyObject,
+  },
+]
 
 const cloneDeep = (value) => {
-  return cloneReference(value)
+
+  for (const { validationr, copy } of validations) {
+    if (validationr(value)) return copy(value);
+  }
+
+  return value;
 }
 
 export default cloneDeep;
