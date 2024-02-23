85cd03c01fde2735678b56f095aff9ea2ffae9d5
woong <ckehfqkr302@naver.com>
2024-02-23 17:59:33 +0900

    URL에 따른 정규표현식

--- Router/src/router.js
+++ Router/src/router.js
@@ -1,15 +1,25 @@
-export default function createRouter(routes) {
-  const PATH_PARAMETER = /:([^/]+)/;
+import matchPathParameters from './matchPathParameters.js';
 
+export default function createRouter(routes) {
   const router = {
     start() {
       const checkRoutes = () => {
-        const currentRoute = routes.find(route => route.path === window.location.hash);
-        currentRoute.element();
+        const currentHash = window.location.hash;
+        const currentRoutes = routes.find(route => {
+          const routePath = route.path.replace(/:([^/]+)/g, '([^/]+)');
+          const regex = new RegExp(`^${routePath}$`);
+          return regex.test(currentHash);
+        });
+        if (currentRoutes) {
+          const params = matchPathParameters(currentHash, currentRoutes.path);
+          currentRoutes.element(params);
+        }
       }
+
       window.addEventListener('hashchange', checkRoutes);
       checkRoutes();
     },
+
     navigate(path) {
       window.location.hash = path;
     },
