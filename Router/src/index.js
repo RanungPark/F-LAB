cf5ca3c4b7ecfaa641725f166588ff0b162a1a72
woong <ckehfqkr302@naver.com>
2024-03-02 17:36:28 +0900

    해시 지우기

--- Router/src/index.js
+++ Router/src/index.js
@@ -1,26 +1,27 @@
-import createRouter from "./router.js";
+import Router from "./router.js";
 
-const container = document.querySelector("main")
+const container = document.querySelector("main");
 
-const router = createRouter([
+const router = new Router([
   {
-    path: '#/',
+    path: '/',
     element: () => container.innerText = "home page",
   },
   {
-    path: '#/blog',
+    path: '/blog',
     element: () => container.innerText = "blog page",
   },
   {
-    path: '#/blog/:name/:page',
+    path: '/blog/:name/:page',
     element: (params) => container.innerText = `${params.name} ${params.page}page`,
   },
 ]);
 
-window.addEventListener('click', e => {
+document.addEventListener('click', e => {
   if (e.target.getAttribute("data-navigate")) {
-    router.navigate(e.target.getAttribute("data-navigate"))
+    e.preventDefault();
+    router.navigate(e.target.getAttribute("data-navigate"));
   }
-})
+});
 
 router.start();
