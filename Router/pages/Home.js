0b0529f5da6fe9d3af774e7e0a77bf5f1112c175
woong <ckehfqkr302@naver.com>
2024-03-05 02:38:43 +0900

    innerHTML 추가

--- Router/pages/Home.js
+++ Router/pages/Home.js
@@ -1,10 +1,11 @@
 class Home {
   constructor({ $target }) {
-
     const $page = document.createElement('div')
     $page.className = 'Home'
-
-    $page.innerHTML = '<h1>Home</h1>'
+    $page.innerHTML =
+      `<h1>Home</h1>
+      <p>홈 화면입니다.</p>
+    `
 
     this.render = () => {
       if ($target.firstChild) {
