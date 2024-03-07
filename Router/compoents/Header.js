4e0bf49b7dc5d3fcb570f49ae5a2c34595f82869
woong <ckehfqkr302@naver.com>
2024-03-07 18:04:42 +0900

    헤더 컴포넌트

---
+++ Router/compoents/Header.js
@@ -0,0 +1,24 @@
+function Header({ contentElement }) {
+  const headerElement = document.createElement('nav');
+  headerElement.className = 'navbar';
+  headerElement.innerHTML =
+    `<div class='navbar_content'>
+      <div class='navbar_logo'>tosstech
+      </div>
+      <ul class='navbar_list'>
+        <li class='navbar_item'>
+          <a>디자인</a>
+        </li>
+        <li class='navbar_item'>
+          <a>개발</a>
+        </li>
+        <li class='navbar_item'>
+          <button class='btn'>채용 바로가기</button>
+        </li>
+      </ul>
+    </div>
+  `
+  contentElement.appendChild(headerElement);
+}
+
+export default Header;
