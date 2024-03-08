c1af00bc59bc2358dffd5e78894a816c5ee4b566
woong <ckehfqkr302@naver.com>
2024-03-09 00:54:17 +0900

    리스트 아이템 컴포넌트

---
+++ Router/src/compoents/Item.js
@@ -0,0 +1,17 @@
+function Item({ ListElement, id, title, content, imagePath, date, pathname}) {
+  const ItemElement = document.createElement('li');
+  ItemElement.className = `list_item_${id}`
+  ItemElement.innerHTML =
+    `<a class='list_wrapper' href=${pathname}/article/${id}>
+      <img srcset=${imagePath} alt='thumbnail' class='item_img' decoding="async" loading="lazy"/>
+      <div class='item_info'>
+        <span class="item_title">${title}</span>
+        <span class="item_content">${content}</span>
+        <span class="item_date">${date}</span>
+      </div>
+    </a>
+  `
+  ListElement.appendChild(ItemElement);
+}
+
+export default Item;
