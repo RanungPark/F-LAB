03051bad1465d534f52c8c9d8841b17dd3e2146b
woong <ckehfqkr302@naver.com>
2024-03-09 00:52:45 +0900

    웹팩설정

--- Router/webpack.config.js
+++ Router/webpack.config.js
@@ -1,29 +1,29 @@
-// const path = require('path');
+const path = require('path');
 
-// module.exports = {
-//   entry: ['@babel/polyfill', './src/App.js'], // 진입점(entry point) 파일 경로
-//   output: {
-//     path: path.resolve(__dirname, 'dist'), // 번들링된 파일의 출력 경로
-//     filename: 'js/bundle.js' // 번들링된 파일의 이름
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/, // JavaScript 파일에 대한 로더를 설정
-//         include: [
-//           path.resolve(__dirname, 'src/js')
-//         ],
-//         exclude: /node_modules/, // 번들링에서 제외할 경로
-//         use: {
-//           loader: 'babel-loader', // 바벨 로더를 사용하여 ES6+ 코드를 변환
-//           options: {
-//             presets: ['@babel/preset-env'],
-//             plugins: ['@babel/plugin-proposal-class-properties']
-//           },
-//         }
-//       }
-//     ]
-//   },
-//   devtool: 'source-map',
-//   mode: 'development'
-// };
+module.exports = {
+  entry: ['@babel/polyfill', './src/App.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'src/js')
+        ],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          },
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  mode: 'development'
+};
