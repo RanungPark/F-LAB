f9d817eb541a4bae8f25fb2be9548fdc31391cc9
woong <ckehfqkr302@naver.com>
2024-03-10 00:23:23 +0900

    주소 수정

--- Router/src/compoents/Item.js
+++ Router/src/compoents/Item.js
@@ -1,8 +1,8 @@
-function Item({ ListElement, id, title, content, imagePath, date, pathname}) {
+function Item({ ListElement, id, title, content, imagePath, date, pathname }) {
   const ItemElement = document.createElement('li');
   ItemElement.className = `list_item_${id}`
   ItemElement.innerHTML =
-    `<a class='list_wrapper' href=${pathname}/article/${id}>
+    `<a class='list_wrapper' href=${pathname === '/' || pathname === '/tech' ? 'tech' : 'design'}/article/${id}>
       <img srcset=${imagePath} alt='thumbnail' class='item_img' decoding="async" loading="lazy"/>
       <div class='item_info'>
         <span class="item_title">${title}</span>
