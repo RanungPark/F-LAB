b4fe9081caf2b67db7b2946610b4e2b09f40cda3
woong <ckehfqkr302@naver.com>
2024-03-12 00:04:32 +0900

    세션 컴포넌트

---
+++ Router/src/compoents/Section.js
@@ -0,0 +1,18 @@
+function Section({ contentElement }) {
+  const sectionElement = document.createElement('section');
+  sectionElement.className = 'section';
+  sectionElement.innerHTML = `
+  <image class='section_img' src="https://static.toss.im/3d/website_code_blue_alpha.png" decoding="async">
+  <div class='section_content'>
+    <div class='section_title'>토스팀이 만드는 수많은 혁신의 순간들</div>
+    <div class='section_sentence'>
+      <span>당신과 함께 만들고 싶습니다.</span>
+      <span>지금, 토스팀에 합류하세요.</span>
+    </div>
+    <button class='btn'>채용 중인 공고 보기</button>
+  </div>
+  `
+  contentElement.appendChild(sectionElement);
+}
+
+export default Section
