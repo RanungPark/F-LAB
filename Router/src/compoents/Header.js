d5d51ba1b73696b0ccdba642650357d76bdb06b9
woong <ckehfqkr302@naver.com>
2024-03-11 14:00:48 +0900

    토스로고 추가

--- Router/src/compoents/Header.js
+++ Router/src/compoents/Header.js
@@ -3,7 +3,10 @@ function Header({ contentElement }) {
   headerElement.className = 'navbar';
   headerElement.innerHTML =
     `<div class='navbar_content'>
-      <div class='navbar_logo'>tosstech
+      <div class='navbar_logo'>
+        <img src='/src/mocks/Toss_Symbol_Primary.png' class='log_img' decoding='async'>
+        <span class='log_bold'>toss</span>
+        <span>tech</span>
       </div>
       <ul class='navbar_list'>
         <li class='navbar_item'>
