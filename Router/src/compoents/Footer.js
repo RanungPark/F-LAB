05e657996b06ce180d0c558414af16d2b568bd3b
woong <ckehfqkr302@naver.com>
2024-03-18 23:29:19 +0900

    fetch함수 따로 뺴기

--- Router/src/compoents/Footer.js
+++ Router/src/compoents/Footer.js
@@ -1,52 +1,54 @@
+import { dataPath } from '../common/dataPath.js';
+import { createElementHelper } from '../helpers/element.helpers.js';
 import { blogData } from '../mocks/data.js';
 import BlogImg from './Blogimg.js';
 
-async function Footer({ contentElement }) {
-  const footerElement = document.createElement('footer');
-  footerElement.className = 'footer';
-
-  const footerWapperElement = document.createElement('div');
-  footerWapperElement.className = 'footer_wapper';
+function Footer({ contentElement }) {
+  const footerElement = createElementHelper('footer', 'footer');
+  const footerWapperElement = createElementHelper('div', 'footer_wapper');
+  const blogElement = createElementHelper('div', 'blog');
 
   footerWapperElement.innerHTML = `
-  <div class='footer_connect_wapper'>
-    <div class='footer_connect'>
-      <span class='footer_connect_title'>토스테크</span>
-      <span>의견 보내기</span>
+    <div class='footer_connect_wapper'>
+      <div class='footer_connect'>
+        <span class='footer_connect_title'>토스테크</span>
+        <span>의견 보내기</span>
+      </div>
+      <div class='footer_connect'>
+        <span class='footer_connect_title'>토스</span>
+        <span>홈페이지</span>
+        <span>회사 소개</span>
+        <span>채용</span>
+      </div>
+      <div class='footer_connect'>
+        <span class='footer_connect_title'>고객센터</span>
+        <span>전화: 1599-4905 (24시간 연중무휴)</span>
+        <span>이메일: support@toss.im</span>
+        <span>카카오톡: @toss</span>
+      </div>
     </div>
-    <div class='footer_connect'>
-      <span class='footer_connect_title'>토스</span>
-      <span>홈페이지</span>
-      <span>회사 소개</span>
-      <span>채용</span>
+    <div class='footer_info'>
+      <span class='footer_info_title'>㈜비바리퍼블리카</span>
+      <span>Copyright © Viva Republica, Inc. All Rights Reserved.</span>
     </div>
-    <div class='footer_connect'>
-      <span class='footer_connect_title'>고객센터</span>
-      <span>전화: 1599-4905 (24시간 연중무휴)</span>
-      <span>이메일: support@toss.im</span>
-      <span>카카오톡: @toss</span>
-    </div>
-  </div>
-  <div class='footer_info'>
-    <span class='footer_info_title'>㈜비바리퍼블리카</span>
-    <span>Copyright © Viva Republica, Inc. All Rights Reserved.</span>
-  </div>
   `
+  fetchFun(dataPath.blogDatas, blogElement)
+    .then(footerWapperElement.appendChild(blogElement))
+    .then(footerElement.appendChild(footerWapperElement))
+    .then(contentElement.appendChild(footerElement))
+}
 
-  const blogElement = document.createElement('div');
-  blogElement.className = 'blog';
-
-  fetch('../../public/data/blogdatas.json')
+function fetchFun(path, blogElement) {
+  return fetch(path)
     .then((response) => response.json())
     .then((response) => {
       for (const key in response.data) {
         BlogImg({ blogElement, imagePath: blogData[key] })
       }
     })
-
-  footerWapperElement.appendChild(blogElement);
-  footerElement.appendChild(footerWapperElement);
-  contentElement.appendChild(footerElement);
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });;
 }
 
 export default Footer;
--- Router/src/compoents/Main.js
+++ Router/src/compoents/Main.js
@@ -1,36 +1,35 @@
-import { designDatas, techDatas } from '../mocks/data.js';
+import { dataPath } from '../common/dataPath.js';
+import { createElementHelper } from '../helpers/element.helpers.js';
 import Item from './Item.js';
 
 function Main({ contentElement }) {
   const { pathname } = location;
-
-  const mainElement = document.createElement('main');
-  mainElement.className = 'main';
-
-  const ListElement = document.createElement('ul');
-  ListElement.className = 'mainList';
+  const mainElement = createElementHelper('main', 'main');
+  const ListElement = createElementHelper('ul', 'mainList');
+  const path = pathname === '/' || pathname === '/tech' ? dataPath.techDatas : dataPath.designDatas;;
 
   mainElement.innerHTML = `
-    <span class = 'mainTitle'>${pathname === '/' || pathname === '/tech' ? '개발' : '디자인'}</span>
+    <span class = 'mainTitle'>
+      ${pathname === '/' || pathname === '/tech' ? '개발' : '디자인'}
+    </span>
   `
-  mainElement.appendChild(ListElement);
 
-  const url = pathname === '/' || pathname === '/tech' ? '../../public/data/techdatas.json' : '../../public/data/designdatas.json';
-
-  fetchFun(url, ListElement, pathname)
-
-  contentElement.appendChild(mainElement);
+  fetchFun(path, ListElement, pathname)
+    .then(mainElement.appendChild(ListElement))
+    .then(contentElement.appendChild(mainElement))
 }
 
-
-function fetchFun(url, ListElement, pathname) {
-  return fetch(url)
+function fetchFun(path, ListElement, pathname) {
+  return fetch(path)
     .then((response) => response.json())
     .then((response) => {
       response.data.map(({ id, title, content, imagePath, date }) => Item({
         ListElement, id, title, content, imagePath, date, pathname
       }));
-    });
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });;
 }
 
 export default Main;
