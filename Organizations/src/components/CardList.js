d0d3c32ac1266eea14a8c7007c923748974281af
woong <ckehfqkr302@naver.com>
2024-04-02 03:10:20 +0900

    카드 리스트 컴포넌트

---
+++ Organizations/src/components/CardList.js
@@ -0,0 +1,24 @@
+import { Card } from './Card.js';
+import { Component } from './Component.js';
+
+export class CardList extends Component {
+  render(target, state) {
+    const setElementArr = [
+      {
+        tag: 'main',
+        id: 'main-wrapper',
+      },
+      {
+        tag: 'ul',
+        id: 'card-list'
+      },
+    ]
+    const [mainWrapper, cardList] = this.setElements(setElementArr)
+
+    state.forEach(card => {
+      new Card().render(cardList, { state: card });
+    })
+
+    this.appendChildElement(target, mainWrapper, cardList)
+  }
+}
