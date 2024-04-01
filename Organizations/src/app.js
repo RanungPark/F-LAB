e2a5338d2319efcb64ee94999694eeea1a5d43c7
woong <ckehfqkr302@naver.com>
2024-04-02 03:09:05 +0900

    컴포넌트 변경

--- Organizations/src/app.js
+++ Organizations/src/app.js
@@ -1,11 +1,11 @@
 import { CardList } from './Components/CardList.js';
-import { SideTable } from './Components/SideTable.js';
+import { SideMenu } from './Components/SiedMenu.js';
 import { event } from './libs/eventBus.js';
 
 export default function app(target) {
-  const sideTable = new SideTable();
-  const cardList = new CardList();
+  const newSideMewnu = new SideMenu();
+  const newCardList = new CardList();
 
-  sideTable.render(target);
-  cardList.render(target, event.getState().cards)
+  newSideMewnu.render(target);
+  newCardList.render(target, event.getState().cards)
 }
