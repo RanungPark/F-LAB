4211e219addf10f67685a36f5da4823b90846ef4
woong <ckehfqkr302@naver.com>
2024-04-02 03:09:37 +0900

    getElements로 리팩토링

--- Organizations/src/components/Card.js
+++ Organizations/src/components/Card.js
@@ -1,41 +1,37 @@
-import { imageRouter } from '../data/imageRouter.js';
+import { iconClassName } from '../data/iconClassName.js';
 import { CardBodyInfo } from './CardBodyInfo.js';
 import { Component } from './Component.js';
 
 export class Card extends Component {
-  constructor() {
-    super()
-  }
-
   render(target, { state }) {
     const card = this.setRoot('card');
     card.id = 'card'
     card.className = `card-${state.id}`
 
-    const profile = this.getElement('profile');
-    const headerInfo = this.getElement('header-info');
-    const team = this.getElement('team');
-    const bodyInfo = this.getElement('body-info');
+    const getElementArr = ['.profile', '.header-info', '.team', '.body-info']
+    const [profile, headerInfo, team, bodyInfo] = this.getElements(getElementArr)
 
-    this.setImageElement({ target: profile, className: 'profile-image', src: imageRouter.user });
+    this.setIconElement({ target: profile, className: iconClassName.user });
 
     for (const key in state.headerInfo) {
       const className = `header-info-${key}`;
       const content = state.headerInfo[key];
+
       const headerInfoLine = this.setElement('span', { className, content });
+
       this.appendChildElement(headerInfo, headerInfoLine)
     }
 
     team.textContent = state.team
 
-    for (const key in imageRouter) {
+    for (const key in iconClassName) {
       const keyInText = 'body_info_'
       if (key.includes(keyInText)) {
-        console.log(key)
-        const newCardBodyInfo = new CardBodyInfo();
         const bodyInfoLine = this.setElement('div', { id: 'body-info-line' })
         const content = state.bodyInfo[key.slice(keyInText.length)]
-        newCardBodyInfo.render(bodyInfoLine, key, imageRouter[key], content)
+
+        new CardBodyInfo().render(bodyInfoLine, iconClassName[key], content)
+
         this.appendChildElement(bodyInfo, bodyInfoLine)
       }
     }
