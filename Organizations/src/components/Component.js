3ab46b5e4213a675b1d63dbbd965c1387e9307e2
woong <ckehfqkr302@naver.com>
2024-04-02 03:10:35 +0900

    추가된 함수

--- Organizations/src/components/Component.js
+++ Organizations/src/components/Component.js
@@ -5,10 +5,54 @@ export class Component {
 
   setRoot(id) {
     this.root = document.getElementById(id).content.firstElementChild.cloneNode(true);
-    return this.root
+    return this.root;
   }
 
-  getElement(className) {
-    return this.root.querySelector(`.${className}`);
+  getElements(selectors) {
+    const element = [];
+    selectors.forEach(selector => element.push(this.root.querySelector(selector)))
+
+    return element;
+  }
+
+  getElement(selector) {
+    return this.root.querySelector(selector);
+  }
+
+  setElements(elementArrs) {
+    const elements = []
+    elementArrs.forEach(({ tag, id = null, className = null, content = null }) => {
+      const element = document.createElement(tag);
+      className ? element.className = className : element.id = id;
+      if (content) element.textContent = content
+      elements.push(element)
+    })
+
+    return elements;
+  }
+
+  setElement(tag, { className = null, id = null, content = null }) {
+    const element = document.createElement(tag);
+    className ? element.className = className : element.id = id;
+    if (content) element.textContent = content
+    return element;
+  }
+
+  appendChildElement(target, targetchild = null, ...elements) {
+    if (targetchild) {
+      elements.forEach(element => targetchild.appendChild(element));
+      target.appendChild(targetchild);
+    } else {
+      elements.forEach(element => target.appendChild(element));
+    }
+  }
+
+  setIconElement({ target = null, id = null, className }) {
+    const imageWrapper = target ? target : this.setElement('div', { id });
+    const imageElement = document.createElement('i');
+    imageElement.className = className;
+
+    this.appendChildElement(imageWrapper, imageElement);
+    return imageWrapper;
   }
 }
