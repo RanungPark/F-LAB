36219dbefcabda5c3badae362979e72d91176f2d
woong <ckehfqkr302@naver.com>
2024-04-03 00:22:07 +0900

    이벤트 구독 등록

--- Organizations/src/app.js
+++ Organizations/src/app.js
@@ -1,11 +1,27 @@
 import { CardList } from './Components/CardList.js';
 import { SideMenu } from './Components/SiedMenu.js';
 import { event } from './libs/eventBus.js';
+import { getcard } from './libs/getcard.js';
 
-export default function app(target) {
+export default function app() {
+  const { cards } = event.getState()
+  const target = document.createElement('div')
+  target.id = 'target'
+
   const newSideMewnu = new SideMenu();
   const newCardList = new CardList();
 
+
   newSideMewnu.render(target);
-  newCardList.render(target, event.getState().cards)
+  newCardList.render(target, cards);
+
+  const app = document.getElementById('app')
+
+  if (app.firstChild) {
+    app.removeChild(app.firstChild)
+  }
+  app.appendChild(target)
 }
+
+event.subscribe(getcard, 'model')
+event.subscribe(app, 'view')
