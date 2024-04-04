d43c92bc8e604901db7ccfa060425b40821f39a2
woong <ckehfqkr302@naver.com>
2024-04-04 22:56:40 +0900

    트리 인풋 컴포넌트

--- Organizations/src/components/TreeMenuInput.js
+++ Organizations/src/components/TreeMenuInput.js
@@ -1,14 +1,14 @@
-import { Component } from './Component.js';
+import { Component } from "./Component.js";
 
 export class TreeMenuInput extends Component {
   render(title) {
-    const treeMenuInput = this.setRoot('tree-menu-input')
-    
-    const getElementArr = ['input', 'label']
+    const treeMenuInput = this.setRoot("tree-menu-input")
+
+    const getElementArr = ["input", "label"]
     const [input, label] = this.getElements(getElementArr);
 
     input.id = title;
-    label.setAttribute('for', title)
+    label.setAttribute("for", title)
     label.textContent = title
 
     return treeMenuInput
