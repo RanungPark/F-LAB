4a302aceaafad4997038a37d223f596cf6c2b541
woong <ckehfqkr302@naver.com>
2024-04-04 22:55:26 +0900

    사이드 메뉴 컴포넌트

--- Organizations/src/components/SiedMenu.js
+++ Organizations/src/components/SiedMenu.js
@@ -1,27 +1,27 @@
-import { Component } from './Component.js';
-import { TreeMenus } from './TreeMenus.js';
+import { Component } from "./Component.js";
+import { TreeMenus } from "./TreeMenus.js";
 
 export class SideMenu extends Component {
   render(target, state) {
     const setElementArr = [
       {
-        tag: 'aside',
-        id: 'aside-wrapper',
+        tag: "aside",
+        id: "aside-wrapper",
       },
       {
-        tag: 'div',
-        id: 'tree-menu-header',
-        content: '조직도'
+        tag: "div",
+        id: "tree-menu-header",
+        content: "조직도"
       },
       {
-        tag: 'div',
-        id: 'tree-menu-body'
+        tag: "div",
+        id: "tree-menu-body"
       }
     ]
 
     const [asideWrapper, treeMenuHeader, treeMenuBody] = this.setElements(setElementArr)
 
-    new TreeMenus().render(treeMenuBody)
+    new TreeMenus().render(treeMenuBody, state)
 
     this.appendChildElement(target, asideWrapper, treeMenuHeader, treeMenuBody)
   }
