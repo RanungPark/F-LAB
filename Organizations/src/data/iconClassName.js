4c6b21aa5c07a77682a3d09ee843ea3b83bd9b1f
woong <ckehfqkr302@naver.com>
2024-04-04 22:57:59 +0900

    클레스 네임 경로

--- Organizations/src/data/iconClassName.js
+++ Organizations/src/data/iconClassName.js
@@ -1,6 +1,6 @@
 export const iconClassName = {
-  body_info_message: 'bi bi-envelope',
-  body_info_mobile: 'bi bi-phone',
-  body_info_phone: 'bi bi-telephone-fill',
-  user: 'bi bi-person'
+  body_info_message: "bi bi-envelope",
+  body_info_mobile: "bi bi-phone",
+  body_info_phone: "bi bi-telephone-fill",
+  user: "bi bi-person"
 }
