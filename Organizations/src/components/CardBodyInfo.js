5c2a931c507af28d421e006395667c901a10b26f
woong <ckehfqkr302@naver.com>
2024-04-04 22:53:01 +0900

    카드 바디 정보 컴포넌트

--- Organizations/src/components/CardBodyInfo.js
+++ Organizations/src/components/CardBodyInfo.js
@@ -1,13 +1,13 @@
-import { Component } from './Component.js';
+import { Component } from "./Component.js";
 
 export class CardBodyInfo extends Component {
   render(target, value, content) {
-    const id = 'card-body-info-image';
+    const id = "card-body-info-image";
     const className = value;
     const contentId = `card-body-info-content`
 
     const ImageElement = this.setIconElement({ id, className, src: value });
-    const contentElement = this.setElement('div', { id: contentId, content })
-    this.appendChildElement(target, '' , ImageElement, contentElement)
+    const contentElement = this.setElement("div", { id: contentId, content })
+    this.appendChildElement(target, "", ImageElement, contentElement)
   }
 }
