1829262a48e650296134ce274b17e72d7e8d355e
woong <ckehfqkr302@naver.com>
2024-04-05 02:18:21 +0900

    이벤트 버스

---
+++ Organizations/src/libs/eventBus.js
@@ -0,0 +1,32 @@
+import { handlers } from "./handlers.js";
+
+let initialState = {
+  teamsList: {},
+  team: '',
+  cards: []
+};
+
+const listeners = []
+
+export const event = {
+  getState() {
+    return initialState;
+  },
+
+  subscribe(listener) {
+    if (typeof listener === "function") {
+      listeners.push(listener)
+    }
+  },
+
+  dispatch(action) {
+    if (typeof action === "object" && action.type) {
+      const handler = handlers[action.type];
+      if (handler) {
+        initialState = { ...handler(initialState, action) };
+        console.log(initialState)
+        listeners.forEach(listener => listener())
+      }
+    }
+  }
+}
