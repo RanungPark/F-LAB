54146fb0476448dfeec7324eb14d539fee576c78
woong <ckehfqkr302@naver.com>
2024-04-04 22:56:59 +0900

    트리 메뉴 컴포넌트

--- Organizations/src/components/TreeMenus.js
+++ Organizations/src/components/TreeMenus.js
@@ -1,11 +1,11 @@
-import { teams } from '../data/teams.js';
-import { Component } from './Component.js';
-import { TreeMenu } from './TreeMenu.js';
+import { teams } from "../data/teams.js";
+import { Component } from "./Component.js";
+import { TreeMenu } from "./TreeMenu.js";
 
 export class TreeMenus extends Component {
-  render(target) {
-    const treeMenus = this.setElement('ul', { className: 'tree-menus' })
-    const newTreeMenu = new TreeMenu().render(teams)
+  render(target, state) {
+    const treeMenus = this.setElement("ul", { className: "tree-menus" })
+    const newTreeMenu = new TreeMenu().render(state)
 
     this.appendChildElement(target, treeMenus, newTreeMenu)
   }
