07f12938e72c88712a22d48718c1f7fbed111f25
woong <ckehfqkr302@naver.com>
2024-04-05 02:17:30 +0900

    app

--- Organizations/src/app.js
+++ Organizations/src/app.js
@@ -1,27 +1,24 @@
-import { CardList } from './Components/CardList.js';
-import { SideMenu } from './Components/SiedMenu.js';
-import { event } from './libs/eventBus.js';
-import { getcard } from './libs/getcard.js';
+import { CardList } from "./Components/CardList.js";
+import { SideMenu } from "./Components/SiedMenu.js";
+import { event } from "./libs/eventBus.js";
 
 export default function app() {
-  const { cards } = event.getState()
-  const target = document.createElement('div')
-  target.id = 'target'
+  const { cards, teamsList } = event.getState()
+  const target = document.createElement("div")
+  target.id = "target"
 
   const newSideMewnu = new SideMenu();
   const newCardList = new CardList();
 
-
-  newSideMewnu.render(target);
+  if (Object.keys(teamsList).length !== 0) {
+    newSideMewnu.render(target, teamsList);
+  }
   newCardList.render(target, cards);
 
-  const app = document.getElementById('app')
+  const app = document.getElementById("app");
 
   if (app.firstChild) {
-    app.removeChild(app.firstChild)
+    app.removeChild(app.firstChild);
   }
-  app.appendChild(target)
+  app.appendChild(target);
 }
-
-event.subscribe(getcard, 'model')
-event.subscribe(app, 'view')
