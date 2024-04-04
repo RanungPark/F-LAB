421306ebffe0f09bb236bafdecffade0f12a0887
woong <ckehfqkr302@naver.com>
2024-04-04 22:58:14 +0900

    json파일 경로

---
+++ Organizations/src/data/dataPath.js
@@ -0,0 +1,13 @@
+export const dataPath = {
+  Sales1: "../../public/data/Sales1.json",
+  Sales2: "../../public/data/Sales2.json",
+  GA: "../../public/data/GA.json",
+  HR: "../../public/data/HR.json",
+  Backend: "../../public/data/Backend.json",
+  Frontend: "../../public/data/Frontend.json",
+  Mobile: "../../public/data/Mobile.json",
+  Marketing1: "../../public/data/Marketing1.json",
+  Marketing2: "../../public/data/Marketing2.json",
+  CX: "../../public/data/CX.json",
+  TeamsList: "../../public/data/TeamsList.json",
+}
