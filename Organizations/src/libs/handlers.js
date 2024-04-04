0c348a35f8996e5f7b456b0fd24f3b9120dd52e6
woong <ckehfqkr302@naver.com>
2024-04-05 02:19:50 +0900

    핸들러

---
+++ Organizations/src/libs/handlers.js
@@ -0,0 +1,23 @@
+export const handlers = {
+  SELECT_TEAM: (prev, event) => {
+    const { team } = event.payload;
+    return {
+      ...prev,
+      team
+    }
+  },
+  FIX_CardS: (prev, event) => {
+    const { cards } = event.payload;
+    return {
+      ...prev,
+      cards
+    }
+  },
+  FIX_TEAMS_LIST: (prev, event) => {
+    const { teamsList } = event.payload;
+    return {
+      ...prev,
+      teamsList
+    }
+  }
+}
