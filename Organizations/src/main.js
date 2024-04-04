924fa88ea54f01e704b5beab4fb94b1d1f15e5c1
woong <ckehfqkr302@naver.com>
2024-04-05 02:19:27 +0900

    함수 삭제

--- Organizations/src/main.js
+++ Organizations/src/main.js
@@ -1,10 +1,8 @@
 import app from "./app.js";
 import { actions } from './libs/actions.js';
 import { event } from "./libs/eventBus.js";
-import { fixTeamsList } from './libs/fixTeamsList.js';
 import { getBackendDatas } from './libs/getBackendDatas.js';
 
-// fixTeamsList()
 getBackendDatas('TeamsList', actions.FixTeamsList, event.dispatch)
   .then(() => app())
 
