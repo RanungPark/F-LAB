cb6e7e3550ae72afa9bbe9c2fd1527470ce3e687
woong <ckehfqkr302@naver.com>
2024-04-04 22:57:38 +0900

    트리 메뉴 스펜 컴포넌트

--- Organizations/src/components/TreeMenuSpan.js
+++ Organizations/src/components/TreeMenuSpan.js
@@ -1,17 +1,17 @@
-import { actions } from '../libs/actions.js';
-import { event } from '../libs/eventBus.js';
-import { Component } from './Component.js';
+import { actions } from "../libs/actions.js";
+import { event } from "../libs/eventBus.js";
+import { Component } from "./Component.js";
 
 export class TreeMenuSpan extends Component {
   render(childs) {
-    const ulElement = this.setElement('ul', { className: 'ulElement' })
+    const ulElement = this.setElement("ul", { className: "ulElement" })
 
     childs.forEach(child => {
-      if (typeof child === 'string') {
-        const treeMenuSpan = this.setRoot('tree-menu-span')
-        const span = this.getElement('span')
-        span.textContent = child
-        span.addEventListener('click', () => {event.dispatch(actions.selectTeam(child))})
+      if (typeof child.teamCode !== "undefined") {
+        const treeMenuSpan = this.setRoot("tree-menu-span")
+        const span = this.getElement("span")
+        span.textContent = child.teamName
+        span.addEventListener("click", () => { event.dispatch(actions.selectTeam(child.teamCode)) })
         this.appendChildElement(ulElement, treeMenuSpan)
       }
     });
