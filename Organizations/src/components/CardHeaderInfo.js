0a82173ec6067c5a59dd23c5c00ae9dc95558ab9
woong <ckehfqkr302@naver.com>
2024-04-06 21:24:45 +0900

    카드 헤더 인포 컴포넌트

---
+++ Organizations/src/components/CardHeaderInfo.js
@@ -0,0 +1,15 @@
+import { Component } from './Component.js';
+
+export class CardHeaderInfo extends Component {
+  render({ headerInfo }) {
+    const headerInfoLines = [];
+    for (const key in headerInfo) {
+      const className = `header-info-${key}`;
+      const content = headerInfo[key];
+
+      const headerInfoLine = this.setElement("span", { className, content });
+      headerInfoLines.push(headerInfoLine);
+    }
+    return headerInfoLines;
+  }
+}
