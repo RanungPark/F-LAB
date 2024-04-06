6116e20880ab06f561e4cffd20ba331a586aebff
woong <ckehfqkr302@naver.com>
2024-04-06 21:24:33 +0900

    카드 바디 인포 리인 컴포넌트

---
+++ Organizations/src/components/CardBodyInfoLines.js
@@ -0,0 +1,29 @@
+import { iconClassName } from '../data/iconClassName.js';
+import { CardBodyInfo } from './CardBodyInfo.js';
+import { Component } from './Component.js';
+
+export class CardBodyInfoLines extends Component {
+  render({ bodyInfo }) {
+    const bodyInfoLines = [];
+    for (const key in iconClassName) {
+      const keyInText = "body_info_";
+      if (key.includes(keyInText)) {
+        const bodyInfoLine = this.setElement("div", { id: "body-info-line" });
+        const content = bodyInfo[key.slice(keyInText.length)];
+
+        const bodyInfoComponent = new CardBodyInfo();
+        const [bodyInfoIcon, bodyInfoContent] = bodyInfoComponent.render(
+          {
+            target: bodyInfoLine,
+            value: iconClassName[key],
+            content,
+          }
+        );
+
+        this.appendElements(bodyInfoLine, bodyInfoIcon, bodyInfoContent);
+        bodyInfoLines.push(bodyInfoLine);
+      }
+    }
+    return bodyInfoLines;
+  }
+}
