e1452c5146c6cc7c9cc0dd3968d4a636dc02919d
woong <ckehfqkr302@naver.com>
2024-04-06 21:25:40 +0900

    dispath 매개변수로 전달받고 적용

--- Organizations/src/components/TreeMenuSpan.js
+++ Organizations/src/components/TreeMenuSpan.js
@@ -1,21 +1,26 @@
 import { actions } from "../libs/actions.js";
-import { event } from "../libs/eventBus.js";
 import { Component } from "./Component.js";
 
 export class TreeMenuSpan extends Component {
-  render(childs) {
-    const ulElement = this.setElement("ul", { className: "ulElement" })
+  render({ childs, dispatch }) {
+    const TMChiledsWrapper = this.setElement("ul", { className: "ulElement" });
 
     childs.forEach(child => {
       if (typeof child.teamCode !== "undefined") {
-        const treeMenuSpan = this.setRoot("tree-menu-span")
-        const span = this.getElement("span")
-        span.textContent = child.teamName
-        span.addEventListener("click", () => { event.dispatch(actions.selectTeam(child.teamCode)) })
-        this.appendChildElement(ulElement, treeMenuSpan)
+        const treeMenuSpan = this.setRoot("tree-menu-span");
+        const span = this.getElement("span");
+        span.textContent = child.teamName;
+        span.addEventListener("click", () => {
+          dispatch(actions.selectTeam({
+            team: child.teamCode,
+            action: actions.FixCards,
+            dispatch,
+          }))
+        })
+        this.appendElements(TMChiledsWrapper, treeMenuSpan);
       }
     });
 
-    return ulElement
+    return TMChiledsWrapper;
   }
 }
