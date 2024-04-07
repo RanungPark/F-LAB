1a730066790b188ca379d8893fa2f36b898e0772
woong <ckehfqkr302@naver.com>
2024-04-07 18:31:58 +0900

    mail변경

--- Organizations/src/data/iconClassName.js
+++ Organizations/src/data/iconClassName.js
@@ -1,6 +1,6 @@
 export const iconClassName = {
-  body_info_message: "bi bi-envelope",
+  body_info_mail: "bi bi-envelope",
   body_info_mobile: "bi bi-phone",
   body_info_phone: "bi bi-telephone-fill",
-  user: "bi bi-person"
-}
+  user: "bi bi-person",
+};
