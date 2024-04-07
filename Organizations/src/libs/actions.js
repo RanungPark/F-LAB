dd900da11fbc51f057191b9dbd03db9704f0cdaf
woong <ckehfqkr302@naver.com>
2024-04-07 18:31:43 +0900

    엑션 추가

--- Organizations/src/libs/actions.js
+++ Organizations/src/libs/actions.js
@@ -1,23 +1,29 @@
-import { getcard } from './getcard.js'
+import { getBackendDatas } from './getBackendDatas.js'
 
 export const actions = {
-  selectTeam: (team) => {
-    getcard(team)
+  selectTeam: ({ team, action, dispatch }) => {
+    getBackendDatas({ key: team, action, dispatch });
     return {
       type: "SELECT_TEAM",
       payload: { team },
-    }
+    };
   },
   FixCards: (cards) => {
     return {
       type: "FIX_CardS",
       payload: { cards },
-    }
+    };
   },
   FixTeamsList: (teamsList) => {
     return {
       type: "FIX_TEAMS_LIST",
       payload: { teamsList },
+    };
+  },
+  setCardNodes: (cardNodes) => {
+    return {
+      type: 'SET_CARD_NODES',
+      payload: { cardNodes },
     }
   }
-}
+};
